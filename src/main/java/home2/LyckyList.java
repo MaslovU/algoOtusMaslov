0bd6bd9d06dee557f9061a836a2ed1f770a78e14
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2023-02-11 14:49:14 +0500

    polish

--- src/main/java/home2/LyckyList.java
+++ src/main/java/home2/LyckyList.java
@@ -3,15 +3,7 @@ package home2;
 public class LyckyList {
     public static void main(String[] args) {
 
-        int num = 001;
         int count = 0;
-        int res = num % 10;
-        int res2 = num / 10 % 10;
-        int res3 = num /100 % 10;
-
-        System.out.println(res);
-        System.out.println(res2);
-        System.out.println(res3);
 
         for (int i = 0; i < 1000; i++) {
             for (int j = 0; j < 1000; j++) {
@@ -20,7 +12,6 @@ public class LyckyList {
                 if ( resi == resj) count ++;
             }
         }
-
         System.out.println(count);
     }
 }
