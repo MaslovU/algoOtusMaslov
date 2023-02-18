6880bfcbd092c6bba6b9b398d88642eea16e358e
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2023-02-18 09:55:51 +0500

    polish

--- src/main/java/home2/LyckyList.java
+++ src/main/java/home2/LyckyList.java
@@ -1,6 +1,8 @@
 package home2;
 
 public class LyckyList {
+
+    static int countReq;
     public static void main(String[] args) {
 
         int count = 0;
@@ -12,6 +14,24 @@ public class LyckyList {
                 if ( resi == resj) count ++;
             }
         }
-        System.out.println(count);
+
+        requrs(3, 0, 0);
+
+        System.out.println("Count is: " + count);
+        System.out.println("CountReq is: " + countReq);
+    }
+
+    private static void requrs(int num, int sumA, int sumB) {
+        if (num == 0) {
+            if (sumA == sumB) {
+                countReq++;
+            }
+            return;
+        }
+        for (int a = 0; a <=9; a++) {
+            for (int b = 0; b <=9; b++) {
+                requrs(num - 1, sumA, sumB);
+            }
+        }
     }
 }
